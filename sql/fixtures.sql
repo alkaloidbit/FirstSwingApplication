69a7e861ed31b7bcdb30b65bd9384af9c23d4efa
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-06-22 21:41:52 +0200

    Adding init.sh + update code

--- sql/fixtures.sql
+++ sql/fixtures.sql
@@ -1,33 +1,38 @@
 
-USE `gestion_bu`;
+USE `firstswingapp`;
 
 -- Users --
-INSERT INTO `user` (`id_user`, `is_admin`, `first_name`, `last_name`, `email`, `password`) VALUES (NULL, '1', 'Oudjoudi', 'Frédéric', 'fredoudjoudi@gmail.com', 'aliboulala');
-INSERT INTO `user` (`id_user`, `is_admin`, `first_name`, `last_name`, `email`, `password`) VALUES (NULL, '0', 'Roberts', 'Clive', 'cliveroberts@sql-university.com', 'password');
+INSERT INTO `user` (`id_user`, `is_admin`, `first_name`, `last_name`, `email`, `password`) VALUES (NULL, 1, 'Frédéric', 'Oudjoudi', 'fredoudjoudi@gmail.com', 'foo');
+INSERT INTO `user` (`id_user`, `is_admin`, `first_name`, `last_name`, `email`, `password`) VALUES (NULL, 0, 'Clive', 'Roberts', 'cliveroberts@ucsql.com', 'foo');
+INSERT INTO `user` (`id_user`, `is_admin`, `first_name`, `last_name`, `email`, `password`) VALUES (NULL, 1, 'fred', 'oudjoudi', 'fo', 'foo');
+INSERT INTO `user` (`id_user`, `is_admin`, `first_name`, `last_name`, `email`, `password`) VALUES (NULL, 0, 'Clive', 'Roberts', 'cr', 'foo');
 
--- Editor --
 INSERT INTO `edition` (`id_edition`, `name`) VALUES (NULL, 'Hoboken, NJ : John Wiley & Sons, Inc.');
--- Document --
-INSERT INTO `document` (`id_document`, `title`, `pages_nbr`, `id_edition`, `year`) VALUES (NULL, 'Beginning programming with Python ', '392', '1', '2018');
--- author --
-INSERT INTO `author` (`id_author`, `name`, `surname`) VALUES (NULL, 'Mueller', 'John Paul');
--- Now we need to populate the ECRIRE table to associate Document and author
-INSERT INTO `compose` (`id_author`, `id_document`) VALUES ('1', '1');
--- Genre --
 INSERT INTO `genre` (`id_genre`, `name`) VALUES (NULL, 'For dummies');
+INSERT INTO `author` (`id_author`, `first_name`, `last_name`) VALUES (NULL, 'Mueller', 'John Paul');
+INSERT INTO `document` (`id_document`, `title`, `pages_nbr`, `id_edition`, `id_genre`, `year`) VALUES (NULL, 'Beginning programming with Python ', '392', 1, 1,  '2018');
+INSERT INTO `compose` (`id_author`, `id_document`) VALUES (1, 1);
 
-INSERT INTO `belongs` (`id_document`, `id_genre`) VALUES (1, 1);
 
+INSERT INTO `edition` (`id_edition`, `name`) VALUES (NULL, 'Paris: First interactive');
+INSERT INTO `genre` (`id_genre`, `name`) VALUES (NULL, 'Pour les nuls');
+INSERT INTO `document` (`id_document`, `title`, `pages_nbr`, `id_edition`, `id_genre`, `year`) VALUES (NULL, 'Python', '388', 2, 2, '2018');
+INSERT INTO `compose` (`id_author`, `id_document`) VALUES (1, 2);
 
--- Editor --
 INSERT INTO `edition` (`id_edition`, `name`) VALUES (NULL, 'Birmingham, UK : Packt Publishing');
--- Document --
-INSERT INTO `document` (`id_document`, `title`, `pages_nbr`, `id_edition`, `year`) VALUES (
-  NULL, 'Python 3 object-oriented programming ', '452', '2', '2018');
--- author --
-INSERT INTO `author` (`id_author`, `name`, `surname`) VALUES (NULL, 'Dusty', 'Phillips');
--- Now we need to populate the ECRIRE table to associate Document and author
-INSERT INTO `compose` (`id_author`, `id_document`) VALUES ('2', '2');
+INSERT INTO `document` (`id_document`, `title`, `pages_nbr`, `id_edition`, `id_genre`, `year`) VALUES (
+  NULL, 'Python 3 object-oriented programming ', '452', 3, NULL, '2018');
+INSERT INTO `author` (`id_author`, `first_name`, `last_name`) VALUES (NULL, 'Dusty', 'Phillips');
+INSERT INTO `compose` (`id_author`, `id_document`) VALUES (2, 3);
 
 
+INSERT INTO `edition` (`id_edition`, `name`) VALUES (NULL, "New York : Apress");
+INSERT INTO `genre` (`id_genre`, `name`) VALUES (NULL, "The expert's voice in open source");
+INSERT INTO `document` (`id_document`, `title`, `pages_nbr`, `id_edition`, `id_genre`, `year`) VALUES (
+  NULL, 'Dive into Python 3', '360', 4, 3, '2008');
+INSERT INTO `author` (`id_author`, `first_name`, `last_name`) VALUES (NULL, 'Mark', 'Pilgrim');
+INSERT INTO `compose` (`id_author`, `id_document`) VALUES (3, 4);
+
+-- A second author for document 4
+INSERT INTO `compose` (`id_author`, `id_document`) VALUES (2, 4);
 
