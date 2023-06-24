63c06f46d4999fbfca6fcb20263ce8dc5f7b21d2
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-06-24 15:15:14 +0200

    Add PasswordHasher class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import Models.PasswordHasher;
+
 import Models.LoggedUser;
 import Views.DocumentsSearchPage;
 import Views.LoginPage;
@@ -6,6 +8,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedLookAndFeelException {
-        LoginPage loginPage = new LoginPage();
+        // LoginPage loginPage = new LoginPage();
+        PasswordHasher ph = new PasswordHasher();
     }
 }
